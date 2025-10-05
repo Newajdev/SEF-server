767e4dfbf3b90dfcdce5c91e6a621ad9afcfc9d0
Newajdev <newaj.design@gmail.com>
2025-10-05 14:22:45 +0600

    Review Post API creaded

--- index.js
+++ index.js
@@ -21,6 +21,28 @@ app.get('/reviews', async(req, res)=>{
     }
     res.send(AllReviews)
 })
+app.post('/reviews', async (req, res)=>{
+    const {PhotoUrl, FullName, CoursName, Review} = req.body;
+
+    if(!PhotoUrl){
+        res.send({Message: 'Please upload your Profile Image'})
+    } else if(!FullName){
+        res.send({Message: 'Please write your Full Name'})
+    }else if(!CoursName){
+        res.send({Message: 'Please write your Course Name'})
+    }else if(!Review){
+        res.send({Message: 'Please write your Reviews'})
+    }else{
+        const senderReview = new ReviewSchema({
+            photourl:PhotoUrl,
+            fullname:FullName,
+            coursname:CoursName,
+            review:Review
+        })
+        await senderReview.save();
+        res.send({Message: `${FullName} your review is added succesfully..`})
+    }
+})
 
 app.get('/', (req, res) => {
   res.send("Shah Emdadia Freelancers' Server is Running .............")
