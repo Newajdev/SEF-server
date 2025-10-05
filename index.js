8f3a1b0a6b399218ea87d67e216af11c802ecb13
Newajdev <newaj.design@gmail.com>
2025-10-05 13:34:25 +0600

    shah emdadia freelancers server development started

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.x1smjlm.mongodb.net/ShahEmdadiaFreelancers?retryWrites=true&w=majority&appName=Cluster0`)
+.then(()=>{console.log("Shah Emdadia Freelancers Database is Connected")})
+
+app.get('/', (req, res) => {
+  res.send("Shah Emdadia Freelancers' Server is Running .............")
+})
+
+app.listen(port, () => {
+  console.log(`Shah Emdadia Freelancers' Server in Running on port ${port}`)
+})
